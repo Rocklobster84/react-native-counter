3fa21324efce270bc78eddba9e94689a1ce64faa
Stephanie Thompson <43892794+Rocklobster84@users.noreply.github.com>
2024-03-19 16:54:40 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text></Text>
     </View>
   );
 }
