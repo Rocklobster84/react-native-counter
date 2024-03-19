4a0f5e51bab96291c36d4be75e8b331075a08b77
Stephanie Thompson <43892794+Rocklobster84@users.noreply.github.com>
2024-03-19 17:15:40 -0500

    Initial layout of app without styling

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity, ViewComponent} from 'react-native';
 import React, {useState} from 'react';
 
 export default function App() {
+  const [counter, setCounter] = useState(0)
+
+  const incrementCounter = () => {
+    setCounter(counter + 1)
+  }
+  const decrementCounter = () => {
+    if(counter > 0) {
+      setCounter(counter - 1)
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <View style={styles.counterContainer}>
+        <Text style={styles.counterHead}>Let's count some stuff!</Text>
+        <Text style={styles.counterText}>{counter}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={incrementCounter}
+          style={[styles.button, styles.incrementButton]}
+        >
+        <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={incrementCounter}
+          style={[styles.button, styles.decrementButton, counter === 0 && styles.disabledButton]}
+        >
+          <Text style={styles.buttonText}>-</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -16,4 +43,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  counterContainer: {
+
+  },
+  counterHead: {
+
+  },
+  counterText: {
+
+  },
+  buttonContainer: {
+
+  },
+  button: {
+
+  },
+  incrementButton: {
+
+  },
+  decrementButton: {
+
+  },
+  buttonText: {
+
+  },
+  disabledButton: {
+
+  }
 });
